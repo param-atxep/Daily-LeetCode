d05c31c8aba5deb066a360f91d483b7b6cf420c1
Param Shelke <paramshelke2512@gmail.com>
2026-09-19 10:50:10 +0530

    Time: 0 ms (100%), Space: 47 MB (95.33%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,17 +1,12 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        // Brute Force Approch
-        for(int i = 0 ; i <= nums.length ;i++){
-            boolean flag = false;
-            for(int j = 0 ; j < nums.length ;j++){
-                if(i == nums[j]){
-                    flag = true ;
-                }
-            }
-            if(!flag){
-                return i;
-            }
-        }
-        return -1;
+        // Better Approch
+       int ans = 0;
+       int sum = 0;
+       for(int i = 0 ; i <nums.length ;i++){
+            sum += nums[i];
+            ans += i ;
+       }
+       return ((ans+nums.length)-sum);
         }
 }
